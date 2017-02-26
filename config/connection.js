537ed73e4e8511069c50d7df58069f99c985b755
Chanita Nuansri <nuansric@Chanitas-MacBook-Pro.local>
2017-02-26 15:03:12 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -41,15 +41,6 @@ if (process.env.JAWSDB_URL){
   connectInfo=process.env.JAWSDB_URL;
 }
 
-// Here, we are running the connect method on the connection object. 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
 var connection = mysql.createConnection(connectInfo);
 connection.connect();
 // Now, we are using the 'module.exports' method, saving it as a variable equal to the name of the mysql object.
